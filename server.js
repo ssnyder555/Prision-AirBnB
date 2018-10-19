09dcc16a2d8fa62819f19055b17db641c0d57dc7
Stephen Snyder <ssnyder555@live.com>
2018-10-19 15:55:50 -0500

    server is up

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const app     = express();
 
 
 
@@ -6,3 +8,13 @@
 
 // require our database
 require('./db/db');
+
+
+
+app.get('/', (req, res) => {
+  res.send('This is my Prison App')
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+});
