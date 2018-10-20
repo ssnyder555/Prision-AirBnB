817bfaa3b3e3862708b9b046f9cb5a6a8df8e8d2
Stephen Snyder <ssnyder555@live.com>
2018-10-20 15:06:12 -0500

    got some packages for ya

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-const express = require('express');
-const app     = express();
-
-
-
-
-
-
-// require our database
+// This is where we must Aquire:
+const express         = require('express');
+const app             = express();
+const bodyParser      = require('body-parser');
+const methoodOverride = require('method-override');
+// get the database talking:
 require('./db/db');
 
+// Requiring the middle-ware can me tricky:
+const cellControllers  = require('./controllers/cell');
 
+// make sure to require this before our controller
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methoodOverride('_method'));
 
+//setting up the middle-ware for our controllers
+// where every route will start with /cells
+app.use('/cells', cellsController);
+
+// this thing..? I forgot i need to no
 app.get('/', (req, res) => {
   res.send('This is my Prison App')
 });
 
+// The whole thing listens through here:
 app.listen(3000, () => {
   console.log('Listening on port 3000')
 });
